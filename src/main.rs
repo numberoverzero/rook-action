2337dbd4317610378af19cac981a26e151a0ee6c
Joe Cross <joe.mcross@gmail.com>
2021-11-07 02:08:09 -0800

    hash values to verify content around github secret hiding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sha2::{Digest, Sha256};
 use std::env;
 
 fn main() {
@@ -8,11 +9,15 @@ fn main() {
     }
     write_debug_ln("printing all env vars");
     for (key, value) in env::vars() {
-        write_debug_ln(format!("k:{} v:{}", key, value));
+        write_debug_ln(format!(
+            "k:{} v:{} dig:{:x?}",
+            key,
+            value,
+            hex_sha256(&value)
+        ));
     }
     write_debug_ln("done");
 }
-
 
 fn read_all_args() -> Vec<String> {
     Vec::from_iter(env::args())
@@ -20,4 +25,10 @@ fn read_all_args() -> Vec<String> {
 
 fn write_debug_ln<S: ToString>(x: S) {
     println!("::debug::{}", x.to_string())
+}
+
+fn hex_sha256<S: ToString>(x: &S) -> Vec<u8> {
+    let mut h = Sha256::new();
+    h.update(x.to_string().as_bytes());
+    h.finalize().to_vec()
 }
